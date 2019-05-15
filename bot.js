3cbd9e81417ec12eb07122ef8fc47d26a4ee5caf
Praxivi <33697021+Praxivi@users.noreply.github.com>
2019-05-15 22:19:13 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NTc4MzA3NzE5NjAyMjQxNTY3.XNxtvg.kWF4hsyO1wDzMxvZL4g0Ci2kwAM';
+
+
+const PREFIX = '=';
+
+var version ='1.0.1';
+
+bot.on('ready', () => {
+    console.log('Bot is ready')
+    
+    client.loginprocess.env.BOT_TOKEN);
+})
