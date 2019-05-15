6a5eaf9fadd6f2b8352915e0eb4f6f5234614db6
Praxivi <33697021+Praxivi@users.noreply.github.com>
2019-05-15 22:46:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTc4MzA3NzE5NjAyMjQxNTY3.XNyAoA.7P3rJa6IGcdDmV37jwl7IrbqP4M);
